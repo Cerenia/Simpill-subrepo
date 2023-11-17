8f3b8a9388400c8c5f61c2f733c4e924448212f3
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-11-17 17:55:03 +0100

    initialized new button with empty function

--- AddOnMethods.kt
+++ AddOnMethods.kt
@@ -1,2 +1,28 @@
 package com.example.simpill.ext
 
+import android.app.Activity
+import android.content.Context
+import android.widget.Button
+import com.example.simpill.R
+
+/**
+ * To feed into python script, add around every function that is called from external repository.
+ */
+
+val MARKER_START = "IXwDmcyAEZEUvkES0IXy144JB SimPillAddOn start"
+val MARKER_END = "IXwDmcyAEZEUvkES0IXy144JB SimPillAddOn end"
+
+
+open class AddOn {
+    companion object {
+        @JvmStatic fun initializeGoToLogButton(button: Button, activity: Activity) : Unit {
+            button.setOnClickListener { openTreatmentLogActivity() };
+
+        }
+
+        @JvmStatic fun openTreatmentLogActivity() : Unit {
+
+        }
+    }
+}
+
