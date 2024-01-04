8b45293c22bfc42f94c644b6acf9c1d8d9cbea0b
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2024-01-04 04:17:53 +0100

    some bugfixes, list still not displayig

--- treatmentLog/TreatmentLogActivity.java
+++ treatmentLog/TreatmentLogActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.simpill.R;
+import com.example.simpill.SharedPrefs;
+import com.example.simpill.Simpill;
 
 
 public class TreatmentLogActivity extends AppCompatActivity {
@@ -14,21 +16,36 @@ public class TreatmentLogActivity extends AppCompatActivity {
     private final TreatmentLogDatabase db = new TreatmentLogDatabase(this, null);
     private RecyclerView logs;
     private TextView title;
+    private final SharedPrefs sharedPrefs = new SharedPrefs(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentViewAndDesign();
         title = findViewById(R.id.title);
-        logs = findViewById(R.id.log);
+        logs = findViewById(R.id.log_list);
         initializeTreatmentLog();
         title.setText(R.string.treatment_log_title);
     }
 
+    private void setContentViewAndDesign() {
+        int theme = sharedPrefs.getThemesPref();
+        if (theme == Simpill.BLUE_THEME) {
+            setTheme(R.style.SimpillAppTheme_BlueBackground);
+        } else if (theme == Simpill.GREY_THEME) {
+            setTheme(R.style.SimpillAppTheme_GreyBackground);
+        } else if (theme == Simpill.BLACK_THEME) {
+            setTheme(R.style.SimpillAppTheme_BlackBackground);
+        } else {
+            setTheme(R.style.SimpillAppTheme_PurpleBackground);
+        }
+        setContentView(R.layout.log_view);
+    }
+
     private void initializeTreatmentLog(){
-        RecyclerView rw = findViewById(R.id.log);
         // Usually this would not happen on the UI thread. Keeping in simple for test repo.
         Adapter a = new Adapter(db.getAllEntries().getList());
-        rw.setAdapter(a);
+        logs.setAdapter(a);
     }
 
     public void randomMethod(){
